66a56931de414fd8c559a817e15866f736c7f491
Steven Cabrera Londoño <32532691+stevenstc@users.noreply.github.com>
2022-01-17 23:59:54 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1725,7 +1725,7 @@ app.post('/api/v1/update/playerdata/:wallet',async(req,res) => {
                 data.BallonSet = req.body.valor;
             }
 
-            if(req.query.consulta === "CupsWin"){
+            if(req.query.clave === "CupsWin"){
                 data.CupsWin = parseInt(req.body.valor);
             }
 
